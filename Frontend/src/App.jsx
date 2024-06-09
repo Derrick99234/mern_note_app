9c7ba96d7ca53924b30f22eec5473430c67ee91a
derrick99234 <pshubomi@gmail.com>
2024-06-09 03:52:58 +0100

    from the frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import Home from "./Pages/Home/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Pages/Login/Login";
 import SignUp from "./Pages/SignUp/SignUp";
 const App = () => {
   const routes = (
     <Router>
       <Routes>
+        <Route path="" exact element={<Login />} />
         <Route path="/login" exact element={<Login />} />
         <Route path="/dashboard" exact element={<Home />} />
-        <Route path="/login" exact element={<Login />} />
         <Route path="/signup" exact element={<SignUp />} />
       </Routes>
     </Router>
