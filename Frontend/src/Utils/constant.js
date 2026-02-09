6e290ce3f81aa7adb23c944f5b503e7db2c0b46e
Derrick <pshubomi@gmail.com>
2026-02-09 18:28:20 +0100

    made some changes

--- Frontend/src/Utils/constant.js
+++ Frontend/src/Utils/constant.js
@@ -1,2 +1,5 @@
 export const BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+  import.meta.env.VITE_API_BASE_URL ||
+  (import.meta.env.DEV
+    ? "http://localhost:8000"
+    : "https://mern-note-app-api.vercel.app");
